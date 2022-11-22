eeea5b76135acb451573f5bd0c42d4bc23e83872
Hugo LIEGEARD <hugo@biyn.media>
2022-11-22 09:11:38 +0100

    Travail du 21/11/22
    Début de la mise en place de AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 
+class Contact {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address?: object;
+  phone?: number;
+  website?: string;
+  company?: object;
+}
+
 /**
  * @Component est ce qu'on appel un décorateur.
  * Il va nous permettre de définir 3 paramètres
@@ -32,6 +43,61 @@ import { Component } from '@angular/core';
  */
 export class AppComponent {
 
-  // Déclarer une variable
+  // -- Déclarer une variable
   title = 'Gestion de Contacts';
+
+  // -- Déclarer un objet javascript (json)
+  unContact: Contact = {
+    // CLE : VALEUR
+      id: 1,
+    name: 'Hugo LIEGEARD',
+    username: 'hugoliegeard',
+    email: 'hugo@biyn.media'
+  }
+
+  // -- Tableau de contacts.
+  // Permet de sauvegarder les contacts de notre application.
+  mesContacts: Contact[] = [
+    {
+      // CLE : VALEUR
+      id: 1,
+      name: 'Hugo LIEGEARD',
+      username: 'hugoliegeard',
+      email: 'hugo@biyn.media'
+    },
+    {
+      // CLE : VALEUR
+      id: 2,
+      name: 'Abdoulaye TOURE',
+      username: 'abdoulayetoure',
+      email: 'abdoulaye@wf3.fr'
+    },
+    {
+      // CLE : VALEUR
+      id: 3,
+      name: 'Victor DELEAU',
+      username: 'victordeleau',
+      email: 'victor@wf3.fr'
+    },
+    {
+      // CLE : VALEUR
+      id: 4,
+      name: 'Sara RAHALI',
+      username: 'sararahali',
+      email: 'sara@wf3.fr'
+    }
+  ];
+
+  /**
+   * Contact choisi par mon utilisateur
+   */
+  contactActif:Contact;
+
+  /**
+   * Permet d'afficher un contact.
+   * @param contact
+   */
+  displayContact(contact: Contact) {
+    this.contactActif = contact;
+  }
 }
